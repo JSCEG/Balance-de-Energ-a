46d8aeeda729ba23943cda77dbcb6d92e1752c4b
JSCEG <javiereg3@gmail.com>
2025-08-01 18:30:43 -0600

    01082025-verificadorv6 Final v.1

--- sankey_config.js
+++ sankey_config.js
@@ -182,7 +182,7 @@ window.sankeyConfig = {
             nodos: [
                 //{ nombre: "SPACER_BIG_4", tipo: "Padre", visible: true, posicion: 0, esEspaciador: true, valorEspaciador: 1000 },
                 // Puedes agregar más nodos padre aquí si lo deseas
-                { nombre: "Industrial", tipo: "Padre", visible: true, posicion: 0, flow: 'snik' },
+                { nombre: "Industrial", tipo: "Padre", visible: true, posicion: 0, flow: 'sink' },
                 { nombre: "Transporte", tipo: "Padre", visible: true, posicion: 1, flow: 'sink' }, // Default para manejar valores +/-         
                 { nombre: "Agropecuario", tipo: "Padre", visible: true, posicion: 2, flow: 'sink' },
                 { nombre: "Comercial", tipo: "Padre", visible: true, posicion: 3, flow: 'sink' },
